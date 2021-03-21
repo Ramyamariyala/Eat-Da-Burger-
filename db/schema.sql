6a92e600c0ad24bf5c8dc8dcea09f83d0df41365
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-03-20 22:19:01 -0400

    corrected the errors in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ USE burgers_db;
 DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT:
+    id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255)NOT NULL,
     devoured BOOL DEFAULT false,
     PRIMARY KEY (id)
